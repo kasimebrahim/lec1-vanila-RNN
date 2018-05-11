0b318046b88c1856a384d1fbe111d8467780b018
kasim <se.kasim.ebrahim@gmail.com>
2018-05-11 18:42:13 +0300

    build word dictionary

--- Dataset.py
+++ Dataset.py
@@ -1,3 +1,5 @@
+import collections
+
 from blinker._utilities import lazy_property
 
 
@@ -5,11 +7,13 @@ class Dataset:
     def __str__(self):
         return "positive and negative sentimental sentences"
 
-    def __init__(self, positive_dataset_path, negative_dataset_path):
-        self._positive_dataset_path = positive_dataset_path;
-        self._negative_dataset_path = negative_dataset_path;
+    def __init__(self, positive_dataset_path, negative_dataset_path, vocabulary_size=10000):
+        self._positive_dataset_path = positive_dataset_path
+        self._negative_dataset_path = negative_dataset_path
+        self._vocabulary_size=vocabulary_size
         self._sentences_list = []
         self._tokenized_sentence_list = []
+        self._word_dictionary = {}
 
     @lazy_property
     def labeled_dataset(self):
@@ -37,6 +41,25 @@ class Dataset:
 
         return tot_data, labels
 
-dataset = Dataset("data/rt-polarity.pos", "data/rt-polarity.neg")
-for l,d in zip(dataset.labeled_dataset[1][:5],dataset.labeled_dataset[0][:5]):
-    print(l,d)
+    @lazy_property
+    def word_dictionary(self):
+        """
+        assigns each word a numerical value
+        builds a dictionary of words and their assigned numerical value
+        :return: tuple of dictionary and word frequency
+        """
+        words = [w for line in self.labeled_dataset[0] for w in line.split()]
+
+        count = [["RARE", -1], ["BEGIN", -2], ["END", -3]]
+        count.extend(collections.Counter(words).most_common(self._vocabulary_size - 1))
+
+        for entry in count:
+            self._word_dictionary[entry[0]] = len(self._word_dictionary)
+        return self._word_dictionary, count
+
+
+dataset = Dataset("data/rt-polarity.pos", "data/rt-polarity.neg", 5000)
+# for l,d in zip(dataset.labeled_dataset[1][:5],dataset.labeled_dataset[0][:5]):
+#     print(l,d)
+print(dataset.word_dictionary[1])
+print(dataset.word_dictionary[0]["ready"])
